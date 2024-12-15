c0e89b0e26d903f27a2c775fd336a73b52254e86
nicolas <nicolassauter97@gmail.com>
2024-12-15 16:14:57 +0100

    updated

--- src/routes/projects/+page.server.js
+++
@@ -1,8 +0,0 @@
-import { connectToDatabase } from '$lib/db';
-
-export async function load() {
-    const db = await connectToDatabase();
-    const projects = await db.collection('projects').find({}).toArray();
-
-    return { projects };
-}
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,14 +1,1 @@
-<script>
-    export let projects;
-</script>
-
 <h1>Projektliste</h1>
-<ul>
-    {#each projects as project}
-        <li>
-            <a href={`/projects/${project._id}`}>{project.name}</a>
-        </li>
-    {/each}
-</ul>
-
-<a href="/projects/create">Neues Projekt erstellen</a>
