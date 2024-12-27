bf86f67d0e8ab063bd1fa1909f82794c78d33126
nicolas <nicolassauter97@gmail.com>
2024-12-27 17:27:02 +0100

    update GROSS

--- src/lib/components/projects.svelte
+++ src/lib/components/projects.svelte
@@ -1,14 +1,48 @@
 <script>
-    let {project} = $props();
+    let { project } = $props();
 </script>
-<div>
-    <img src={project.poster} alt={project.name} />
-    <h3>{project.name}</h3>
-    <p>Beschreibung: {project.description}</p>
-    <p>Startdatum: {project.startDate}</p>
-    <p>Enddatum: {project.endDate}</p>
-    <p>Status: {project.status}</p>
+
+<div class="card bg-dark text-light shadow">
+    <img src={project.poster} class="card-img-top" alt={project.name} />
+    <div class="card-body">
+        <h5>
+            <a href={`/projects/${project._id}`} class="card-title no-underline">{project.name}</a>
+          </h5>
+        <p><strong>Beschreibung:</strong> {project.description}</p>
+        <p><strong>Startdatum:</strong> {project.startDate}</p>
+        <p><strong>Enddatum:</strong> {project.endDate}</p>
+        <p class="card-status"><strong>Status:</strong> {project.status}</p>
+    </div>
+    <div class="card-footer">
+        {#if project.projectlist}
+            <form method="POST" action="?/removeFromFav" use:enhance>
+                <input type="hidden" name="id" value={project._id} />
+                <button class="btn btn-danger">Von Favoritenliste entfernen</button>
+            </form>
+        {:else}
+            <form method="POST" action="?/addToFav" use:enhance>
+                <input type="hidden" name="id" value={project._id} />
+                <button class="btn btn-success">Auf die Favoritenliste</button>
+            </form>
+        {/if}
+    </div>
 </div>
 
-
-
+<style>
+    .container {
+      max-width: 1200px;
+    }
+  
+    .no-underline {
+      text-decoration: none;
+    }
+  
+  
+    .no-underline:hover {
+      text-decoration: underline;
+    }
+   .card-status
+    {
+      color: rgb(160, 151, 154);
+    }
+  </style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,53 +1,30 @@
 <script>
-    import Component from "$lib/components/projects.svelte";
-    let { data } = $props();
-  </script>
-  
-  <div class="container mb-5 mt-5">
-    <div class="container text-center">
-      <h1>Projektliste</h1>
-      <p>Diese Seite dient zur Verwaltung von Projekten. Sie bietet eine Übersicht über aktuelle Projekte, einschliesslich 
-      einer kurzen Beschreibung. Über die Schaltfläche "Neues Projekt erstellen" können neue Projekte hinzugefügt werden.
-    </p>
-      <a href="/projects/create" class="btn btn-dark">Neues Projekt erstellen</a>
-    </div>
-  </div>
+  import Project from "$lib/components/projects.svelte";
+  let { data } = $props();
+</script>
 
-  <div class="container text-left mt-3">
-    <div class="row">
-      {#each data.projects as project}
-        <div class="col-md-3 mb-4">
-          <div class="card bg-dark text-light shadow">
-            <img src={project.poster} class="card-img-top" alt={project.name} />
-            <div class="card-body">
-              <h5>
-                <a href={`/projects/${project._id}`} class="card-title no-underline">{project.name}</a>
-              </h5>
-              <p class="card-text"><strong>Beschreibung:</strong> {project.description}</p>
-              <p class="card-status"><strong>Status:</strong> {project.status}</p>
-            </div>
-          </div>
-        </div>
-      {/each}
+<div class="container mb-5 mt-5">
+<div class="container text-center">
+  <h1>Projektliste</h1>
+  <p>Diese Seite dient zur Verwaltung von Projekten. Sie bietet eine Übersicht über aktuelle Projekte, einschließlich 
+  einer kurzen Beschreibung. Über die Schaltfläche "Neues Projekt erstellen" können neue Projekte hinzugefügt werden.
+</p>
+  <a href="/projects/create" class="btn btn-dark">Neues Projekt erstellen</a>
+</div>
+</div>
+
+<div class="container text-left mt-3">
+<div class="row">
+  {#each data.projects as project}
+    <div class="col-md-3 mb-4">
+      <Project {project} />
     </div>
-    <p><em>Daten und Bilder generiert von ChatGPT und DALL-E</em></p>
-  </div>
-  
-  <style>
-    .container {
-      max-width: 1200px;
-    }
-  
-    .no-underline {
-      text-decoration: none;
-    }
-  
-  
-    .no-underline:hover {
-      text-decoration: underline;
-    }
-   .card-status
-    {
-      color: rgb(255, 0, 85);
-    }
-  </style>
+  {/each}
+</div>
+<p><em>Daten und Bilder generiert von ChatGPT und DALL-E</em></p>
+</div>
+<style>
+.container {
+  max-width: 1200px;
+}
+</style>
