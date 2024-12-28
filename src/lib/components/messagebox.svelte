7a5c60f43d8d3d12eaa7e6290776a4310f3dc20c
nicolas <nicolassauter97@gmail.com>
2024-12-28 20:57:25 +0100

    login guuttt hier

---
+++ src/lib/components/messagebox.svelte
@@ -0,0 +1,18 @@
+<script>
+    let {message,name} = $props();
+    //let {nomessage= "No message"} = $props();
+</script>
+
+<div>
+    <p>Your message:</p>
+    <p> {message}</p>
+    <p> {name}</p>
+</div>
+
+<style>
+    div {
+        border: 2px solid black;
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
+    </style>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    import Messagebox from "$lib/components/messagebox.svelte";
+//Formular
+let userform = $state({
+    vorname: "",
+    nachname: ""  
+})
+function handleData() {
+    alert("Login erfolgreich " + userform.vorname + " " + userform.nachname);
+}
+</script>
+
+    <h1>Welcome</h1>
+
+    <!-- Formular --> 
+    <form onsubmit={handleData}>
+        <div class=mb-3>
+            <label class="form-label" for="">Benutzername</label>
+            <input bind:value={userform.vorname} class = "form-control" type="text"> 
+        </div>
+
+        <div class=mb-3>
+            <label class="form-label" for="">Passowrt</label>
+            <input bind:value={userform.nachname} class = "form-control" type="text"> 
+        </div>
+
+        <button type="submit" class= "btn btn-primary"> Submit </button>
+
+    </form>
