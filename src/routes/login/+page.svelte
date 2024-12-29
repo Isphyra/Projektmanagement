4587d42e46dd4a7b0631c653abe339cda35363d5
nicolas <nicolassauter97@gmail.com>
2024-12-29 14:48:23 +0100

    login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,29 +1,33 @@
 <script>
-    import Messagebox from "$lib/components/messagebox.svelte";
-//Formular
-let userform = $state({
-    vorname: "",
-    nachname: ""  
-})
-function handleData() {
-    alert("Login war erfolgreich " + userform.vorname);
-}
+  import Messagebox from "$lib/components/messagebox.svelte";
+
+  // Formular
+  let userform = $state({
+      vorname: "",
+      nachname: ""  
+  });
+
+  function handleData() {
+      alert("Login war erfolgreich " + userform.vorname);
+  }
 </script>
 
-    <h1>Welcome</h1>
+<div class="container mb-3 mt-3">
+  <h1>Willkommen</h1>
+  <div class="container text-left ml-5">
+      <!-- Formular -->  
+      <form onsubmit={handleData}>
+          <div class="mb-3">
+              <label class="form-label" for="">Benutzername</label>
+              <input bind:value={userform.vorname} class="form-control" type="text"> 
+          </div>
 
-    <!-- Formular --> 
-    <form onsubmit={handleData}>
-        <div class=mb-3>
-            <label class="form-label" for="">Benutzername</label>
-            <input bind:value={userform.vorname} class = "form-control" type="text"> 
-        </div>
+          <div class="mb-3">
+              <label class="form-label" for="">Passwort</label>
+              <input bind:value={userform.nachname} class="form-control" type="password"> 
+          </div>
 
-        <div class=mb-3>
-            <label class="form-label" for="">Passwort</label>
-            <input bind:value={userform.nachname} class = "form-control" type="text"> 
-        </div>
-
-        <button type="submit" class= "btn btn-primary"> Login </button>
-
-    </form>
+          <button type="submit" class="btn btn-primary"> Login </button>
+      </form>
+  </div>
+</div>
