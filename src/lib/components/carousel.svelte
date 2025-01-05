8ea24e3b7760e7f99793311ab1b8bbc03d2961bf
nicolas <nicolassauter97@gmail.com>
2025-01-05 22:52:42 +0100

    carousell

---
+++ src/lib/components/carousel.svelte
@@ -0,0 +1,25 @@
+<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="images/startseite.webp" class="d-block w-100" alt="startseite1">
+        </div>
+        <div class="carousel-item">
+            <img src="img/sphinx.jpg" class="d-block w-100" alt="startseite2">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+<style>
+.carousel-inner img {
+    object-fit: cover;
+    height: 400px;
+}
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <script>
     import CompInfoText from "$lib/components/CompInfoText.svelte";
     import LoginButton from "$lib/components/LoginButton.svelte";
+    import Carousel from "$lib/components/carousel.svelte";
 </script>
 
 
     <div class="container text-center mb-5 mt-5">
-    <img src = "/images/startseite.webp" alt = "Startseite" width="400" height="400" class="mt-5"/>
+      <Carousel></Carousel>
       <h1 class="mb-3 mt-5">Protoyping LN2</h1>
       <h2>Projekt von Nicolas Stanislas Sauter</h2>
       <LoginButton></LoginButton>
